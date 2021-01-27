e4d541c25a9ceeef7356f41416b9d7050f83c2a4
JustinV879 <72768893+JustinV879@users.noreply.github.com>
2021-01-27 10:33:21 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-
+while True:
+    print("Rotation of Degrees x axis" + input.rotation(Rotation.PITCH))
+    if (input.rotation(Rotation.PITCH)) > 15:
+        light.set_all(light.rgb(255,255,255))
+    else:
+        light.clear()
--- main.ts
+++ main.ts
@@ -1,1 +1,9 @@
-
+while (true) {
+    console.log("Rotation of Degrees x axis" + input.rotation(Rotation.Pitch))
+    if (input.rotation(Rotation.Pitch) > 15) {
+        light.setAll(light.rgb(255, 255, 255))
+    } else {
+        light.clear()
+    }
+    
+}
